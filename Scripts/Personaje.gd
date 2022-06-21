79cbd15569a8642b6fcfc3411a2d06d52429d78c
Cristian Ramanzin <ramanzincristian@gmail.com>
2022-06-21 17:53:08 -0300

    Clase 5 - Animaciones

--- Scripts/Personaje.gd
+++ Scripts/Personaje.gd
@@ -2,19 +2,24 @@ extends KinematicBody2D
 
 export (int) var velocidad = 600
 
+onready var playback = $AnimationTree.get("parameters/playback")
+
 var movimiento = Vector2(0,0)
 	
 func get_inputs():
 	movimiento = Vector2()
 	if Input.is_action_pressed("move_right"):
 		movimiento.x += velocidad
+		playback.travel("move_right")
 	if Input.is_action_pressed("move_left"):
 		movimiento.x -= velocidad
+		playback.travel("move_left")
 	if Input.is_action_pressed("move_up"):
 		movimiento.y -= velocidad
 	if Input.is_action_pressed("move_down"):
 		movimiento.y += velocidad
-
+	if movimiento == Vector2():
+		playback.start("RESET")
 func _physics_process(_delta):
 	get_inputs()
 	movimiento = move_and_slide(movimiento)
